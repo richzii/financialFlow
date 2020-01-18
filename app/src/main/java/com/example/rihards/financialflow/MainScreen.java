25eaa8ed375d773b5ecf49ddc1617538feb50316
richzii <rihards.ziverts@gmail.com>
2020-01-18 16:15:17 +0200

    Updated activities
    Each activity updated

--- app/src/main/java/com/example/rihards/financialflow/MainScreen.java
+++ app/src/main/java/com/example/rihards/financialflow/MainScreen.java
@@ -5,19 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainScreen extends AppCompatActivity {
 
     private Button btnInc, btnOut, btnExit;
+    private TextView txtMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
+        txtMonth = (TextView) findViewById(R.id.text_month);
         btnInc = (Button) findViewById(R.id.btn_income);
         btnOut = (Button) findViewById(R.id.btn_outcome);
         btnExit = (Button) findViewById(R.id.btn_exit);
+
+        Calendar calendar = Calendar.getInstance();
+        String dayName = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US);
+        txtMonth.setText(dayName);
 
         btnInc.setOnClickListener(new View.OnClickListener() {
             @Override
